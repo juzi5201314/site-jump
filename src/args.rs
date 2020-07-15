f893f4d7e0e6b00835fcfe5eb4247e6d7798526e
soeur <juzi201314@gmail.com>
2020-07-15 15:41:14 +0800

    基本完成

---
+++ src/args.rs
@@ -0,0 +1,34 @@
+
+use argh::FromArgs;
+
+#[derive(FromArgs, Debug, Clone)]
+/// 简单的网页跳转服务
+pub struct Args {
+    /// html文件目录。默认读取index.html作为主页
+    #[argh(option)]
+    pub www: String,
+
+    /// 是否开启静态文件服务。%www%/static
+    #[argh(switch, short = 's')]
+    pub static_file: bool,
+
+    /// 自定义路由。如路由不含有target，则从query字符串里寻找target。
+    #[argh(option, default = r#"String::from("/{target}")"#)]
+    pub route: String,
+
+    /// 监听地址，默认0.0.0.0
+    #[argh(option, default = r#"String::from("0.0.0.0")"#, short = 'a')]
+    pub bind: String,
+
+    /// 监听端口
+    #[argh(option, default = "7070u16", short = 'p')]
+    pub port: u16,
+
+    /// 安静模式，不输出日志
+    #[argh(switch, short = 'q')]
+    pub quiet: bool,
+
+    /// 记录日志到文件，而不是只输出到控制台
+    #[argh(switch, short = 'l')]
+    pub log_to_file: bool
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,109 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate log;
+
+use std::ops::Add;
+
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, web};
+use anyhow::Result;
+use tera::{Context, Tera};
+use serde::Deserialize;
+
+use crate::args::Args;
+use fern::colors::{ColoredLevelConfig};
+
+use colored::{Colorize, Color};
+
+mod args;
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let args: &Args = &argh::from_env();
+
+    if !args.quiet {
+        init_log(args.log_to_file)?;
+    }
+
+    info!("Start parameter: {}", std::env::args().skip(1).collect::<Vec<String>>().join(" "));
+
+    let addr = format!("{}:{}", args.bind, args.port);
+    let temp_dir = args.www.clone();
+    let use_static_file = args.static_file;
+    let route = args.route.clone();
+
+    info!("Listen on {}", addr);
+
+    HttpServer::new(move || {
+        let mut app = App::new()
+            .route(&route.clone(), web::get().to(handle))
+            .data({
+                let mut tera = Tera::new(&temp_dir.clone().add("/*.html")).expect("初始化Tera失败");
+                tera.autoescape_on(Vec::new());
+                tera
+            });
+
+        if use_static_file {
+            app = app.service(actix_files::Files::new("/static", &temp_dir.clone().add("/static")))
+        }
+
+        app
+    })
+        .bind(&addr)?
+        .run()
+        .await?;
+    Ok(())
+}
+
+#[derive(Deserialize)]
+struct QueryS {
+    pub target: Option<String>
+}
+
+fn handle(req: HttpRequest, tera: web::Data<Tera>, query: web::Query<QueryS>) -> HttpResponse {
+    let target = req.match_info().get("target").map(|s| s.to_owned()).or(query.target.clone());
+
+    let context_target = &urldecode(target.as_ref().unwrap_or(&"null".to_owned()));
+    let mut context = Context::new();
+    context.insert("legal", &url::Url::parse(context_target).is_ok());
+    context.insert("target", context_target);
+
+    let html = tera.render("index.html", &context);
+    match html {
+        Ok(html) => {
+            target.map(|target| info!("go to {}", target));
+            HttpResponse::Ok().content_type("text/html").body(html)
+        },
+        Err(err) => {
+            error!("渲染模板时出现错误: {}", err.to_string());
+            HttpResponse::InternalServerError().body(err.to_string())
+        }
+    }
+}
+
+fn init_log(log_to_file: bool) -> Result<()> {
+    let colors = ColoredLevelConfig::new()
+        .info(fern::colors::Color::Green)
+        .warn(fern::colors::Color::Yellow);
+
+    let mut dispath = fern::Dispatch::new()
+        .format(move |out, message, record|
+            out.finish(format_args!(
+                "[{time}] [{level}]: {message} <{target}>",
+                time = chrono::Local::now().format("%y-%m-%d %H:%M:%S"),
+                level = colors.color(record.level()),
+                message = message,
+                target = record.target().color(Color::Blue)
+            ))
+        )
+        .level(log::LevelFilter::Info)
+        .chain(std::io::stdout());
+
+    if log_to_file {
+        dispath = dispath.chain(fern::log_file("site-jump.log")?);
+    }
+    dispath.apply()?;
+    Ok(())
+}
+
+fn urldecode(url: &str) -> String {
+    url::form_urlencoded::parse(url.as_bytes()).map(|(key, val)| [key, val].concat()).collect::<String>()
 }
